41063769ab3581f119f769c3c93b27efc4982891
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 11:17:46 +0530

    Create Castingexercise.java

---
+++ Castingexercise.java
@@ -0,0 +1,18 @@
+package Practicecodes.Java.Openbook;
+
+public class Castingexercise {
+    public static void main(String[] args) { 
+        //byte b=10; 
+         
+        //int i=b;// will accept - automatic type promotion
+         
+        //byte c=i;// will not accept - because lower type cannot be put in higher
+         
+        //byte d=(byte)i;// Type casting makes it possible to store compatible types.
+         
+        //byte x=10; 
+        //byte y=20; 
+         
+        //byte sum=x*y;
+    }
+}
