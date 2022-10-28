82325724ce9b009645c34bf399f3c2bcd97241ec
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 11:29:16 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,11 @@
+package Practicecodes.Java.Openbook;
+
+public class Vowel {
+    public static void main(String[] args) { 
+        char ch = 'i'; 
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) 
+        System.out.println(ch + " is vowel"); 
+        else
+        System.out.println(ch + " is consonant"); 
+    } 
+}
