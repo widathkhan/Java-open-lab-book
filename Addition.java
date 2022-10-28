c7507059e447e0ab3f1b8cc64e6faa7435d96fef
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 11:09:30 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package Practicecodes.Java.Openbook;
+
+public class Addition {
+    public static void main(String[] args) {
+        int first = 100; 
+        int second = 200; 
+        System.out.println(first + " " + second);
+        int sum = first + second; 
+        System.out.println("The sum is: " + sum); 
+        }
+}
