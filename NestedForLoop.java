e492b4c1050458edad5f37d638e46b8fd13d0e3d
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 11:21:54 +0530

    Create NestedForLoop.java

---
+++ NestedForLoop.java
@@ -0,0 +1,11 @@
+package Practicecodes.Java.Openbook;
+
+public class NestedForLoop {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++){
+                System.out.println("The value of i and j in this run: "+i+" "+j);
+            }
+        }
+    }
+}
