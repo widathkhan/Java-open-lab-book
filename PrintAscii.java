506fe9c22e4f18cc990a441bfabf65e1ef16f6b4
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 11:24:21 +0530

    Create PrintAscii.java

---
+++ PrintAscii.java
@@ -0,0 +1,11 @@
+package Practicecodes.Java.Openbook;
+
+public class PrintAscii {
+    public static void main(String[] args) { 
+        char ch = 'J'; 
+        int ascii = ch;
+        int castAscii = (int) ch; 
+        System.out.println("The ASCII value of " + ch + " is: " + ascii); 
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii); 
+        }
+}
