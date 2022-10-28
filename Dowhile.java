bc63033380458b26198d718eb3d5e8d1adb291ef
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 11:19:03 +0530

    Create Dowhile.java

---
+++ Dowhile.java
@@ -0,0 +1,12 @@
+package Practicecodes.Java.Openbook;
+
+public class Dowhile {
+    public static void main(String[] args) {
+        int i=1;
+
+        do{
+            System.out.println("The value of i is "+i);
+            i++;
+        }while(i<11);
+    }
+}
